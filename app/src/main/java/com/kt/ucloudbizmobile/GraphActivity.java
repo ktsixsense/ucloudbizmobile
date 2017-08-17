0013eddf627f0da98d368bc14aa43be1aabe40ff
ktsixsense <new1234!>
2017-08-17 16:21:55 +0900

    csi graph modified - 0817

--- app/src/main/java/com/kt/ucloudbizmobile/GraphActivity.java
+++ app/src/main/java/com/kt/ucloudbizmobile/GraphActivity.java
@@ -12,6 +12,9 @@ import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.DefaultLabelFormatter;
+import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
+import com.jjoe64.graphview.helper.StaticLabelsFormatter;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -104,16 +107,20 @@ public class GraphActivity extends Activity {
     {
         DataPoint dp[];
 
-        dp = new DataPoint[m_count];
+        dp = new DataPoint[m_gap];
         if(m_gap == 30)
         {
             Calendar c = calendar;
             c.add(Calendar.MINUTE,-30);
-            int gap = 30 / m_count;
-            for(int i =0;i<m_count;i++)
+            //int gap = 30 / m_count;
+            int gap = 1; //  눈금간격 : 1분
+            for(int i =0;i<30;i++)
             {
-                Date d= c.getTime();
-                dp[i] = new DataPoint(d,m_data[i]);
+                    Date d= c.getTime();
+                 if(i>=m_count)
+                     dp[i] = new DataPoint(d,0);
+                else
+                    dp[i] = new DataPoint(d,m_data[i]);
                 c.add(Calendar.MINUTE,gap);
             }
         }
@@ -121,11 +128,15 @@ public class GraphActivity extends Activity {
         {
             Calendar c = calendar;
             c.add(Calendar.MINUTE,-120);
-            int gap = 120 / m_count;
-            for(int i =0;i<m_count;i++)
+           // int gap = 120 / m_count;
+            int gap = 1; //  눈금간격 : 1분
+            for(int i =0;i<120;i++)
             {
                 Date d= c.getTime();
-                dp[i] = new DataPoint(d,m_data[i]);
+                if(i>=m_count)
+                    dp[i] = new DataPoint(d,0);
+                else
+                    dp[i] = new DataPoint(d,m_data[i]);
                 c.add(Calendar.MINUTE,gap);
             }
         }
@@ -133,11 +144,15 @@ public class GraphActivity extends Activity {
         {
             Calendar c = calendar;
             c.add(Calendar.MINUTE,-600);
-            int gap = 600 / m_count;
-            for(int i =0;i<m_count;i++)
+          //  int gap = 600 / m_count;
+            int gap = 1; //  눈금간격 : 1분
+            for(int i =0;i<600;i++)
             {
                 Date d= c.getTime();
-                dp[i] = new DataPoint(d,m_data[i]);
+                if(i>=m_count)
+                    dp[i] = new DataPoint(d,0);
+                else
+                    dp[i] = new DataPoint(d,m_data[i]);
                 c.add(Calendar.MINUTE,gap);
             }
         }
@@ -146,21 +161,39 @@ public class GraphActivity extends Activity {
         GraphView graph = (GraphView) findViewById(R.id.graph_metric1);
         graph.removeAllSeries();;
         graph.addSeries(series);
+        graph.getViewport().setMinX(dp[0].getX());
+        graph.getViewport().setMaxX(dp[m_gap-1].getX());
+       // graph.getViewport().set
+       // graph.getViewport().setXAxisBoundsManual(true);
+
+       StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
+        if(m_gap == 30)
+        {
+            staticLabelsFormatter.setHorizontalLabels(new String[] {"30분전", "20분전", "10분전"});
+        }
+        else if(m_gap == 120)
+        {
+            staticLabelsFormatter.setHorizontalLabels(new String[] {"2시간전", "90분전", "60분전","30분전"});
+        }
+        else if(m_gap == 600)
+        {
+            staticLabelsFormatter.setHorizontalLabels(new String[] {"10시간전", "8시간전", "6시간전","4시간전","2시간전"});
+        }
+       //staticLabelsFormatter.setVerticalLabels(new String[] {"low", "middle", "high"});
+       graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
+
         //graph.
         // set date label formatter
-
-     //   graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(getActivity()));
-     //   graph.getGridLabelRenderer().setNumHorizontalLabels(3); // only 4 because of the space
-
+        /*graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(this));
+        graph.getGridLabelRenderer().setNumHorizontalLabels(3); // only 4 because of the space
 // set manual x bounds to have nice steps
-        graph.getViewport().setMinX(dp[0].getX());
+       graph.getViewport().setMinX(dp[0].getX());
         graph.getViewport().setMaxX(dp[m_count-1].getX());
-     //   graph.getViewport().setXAxisBoundsManual(true);
-
+        graph.getViewport().setXAxisBoundsManual(true);
 // as we use dates as labels, the human rounding to nice readable numbers
 // is not necessary
-       // graph.getGridLabelRenderer().setHumanRounding(false);
-
+        //graph.getGridLabelRenderer().setHumanRounding(false);
+*/
         return true;
     }
 }
